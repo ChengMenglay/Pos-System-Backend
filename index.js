591dae547b4cb45e474d3f44b86c9d53f81974bc
Menglay <cheng.menglay79@gmail.com>
2024-09-02 12:13:09 +0700

    first commit

--- index.js
+++ index.js
@@ -2,15 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  })
-);
-const port = process.env.DB_PORT || 8080;
+app.use(cors());
+const port = 8080;
 app.listen(port, (error) => {
   if (!error) {
     console.log("http://localhost:" + port);
--- util/db.js
+++ util/db.js
@@ -1,17 +1,10 @@
 const mysql = require("mysql");
 const util = require("util");
-// const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "ecommerce",
-//   port: "3306",
-// });
 const db = mysql.createConnection({
-  host: "bjed1kqbocbzt3ngllsp-mysql.services.clever-cloud.com",
-  user: "ui4a0im1iegnevr7",
-  password: "4OqZfRzgCM2DSI1FGUhv",
-  database: "bjed1kqbocbzt3ngllsp",
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "pos_system",
   port: "3306",
 });
 
